a22dba0880e708b67a32c464beb652d47b9bfa1f
mike stedman <github@ravuya.com>
2016-10-11 14:33:58 -0600

    Wrote instapaper pusher, but it doesn't work yet (fails on the third push every time)

--- Program.fs
+++ Program.fs
@@ -1,10 +1,29 @@
 ﻿open HtmlAgilityPack
 open System.IO
+open HttpFs.Client
+open System.Text
+open System
+open Hopac
+open System.Threading
 
-let dumpUrls filename (urls : string list) =
-    use fp = File.CreateText(filename)
+let addToInstapaper username password (urls : string list) =
     for url in urls do
-        fp.WriteLine("{0}", url)
+        try
+            let response = 
+                Request.createUrl Post "https://www.instapaper.com/api/add"
+                |> Request.queryStringItem "username" username
+                |> Request.queryStringItem "password" password
+                |> Request.queryStringItem "url" url
+                |> getResponse
+                |> run
+            if (response.statusCode <> 200 && response.statusCode <> 201) then
+                printfn "Something bad happened: %i" response.statusCode
+            else
+                printfn "Committed url %s to instapaper" url
+            Thread.Sleep(2500) // don't beat up Instapaper
+        with
+            | ex -> printfn "Exception: %s" (ex.GetBaseException()).Message
+    ()
 
 [<EntryPoint>]
 let main argv = 
@@ -20,14 +39,15 @@ let main argv =
         // Then only get long term road test URLs
         |> List.filter(fun url -> url.Contains("long-term-road-test"))
         // And only ones about Yugos...
-        |> List.filter(fun url -> url.Contains("yugo"))
-        // Just in case there are duplicates
-        |> List.distinct
+        |> List.filter(fun url -> url.Contains("yugo"))        
     
     // Re-globalize the URL
     let baseUrl = "http://edmunds.com"
     let fullUrls = List.map(fun x -> baseUrl + x) urls
 
+    let username = "foobar@whatwhat.com"
+    let password = "PASSWORD GOES HERE"
+
     // Write to file
-    dumpUrls "output.txt" fullUrls
+    addToInstapaper username password fullUrls
     0
