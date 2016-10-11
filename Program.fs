47ddd64dae82a660c60b04182835d83d3ac070de
mike stedman <github@ravuya.com>
2016-10-11 15:55:15 -0600

    Changed it so I use a job monad instead of the clunky stuff I did before, which also lets me now return the status code hooray!

--- Program.fs
+++ Program.fs
@@ -13,13 +13,21 @@ let createInstapaperRequest username password url =
     |> Request.timeout 3500<ms>
     |> Request.keepAlive false
 
+let sendRequestToInstapaper (request : Request) = job {
+    let! response = getResponse request
+    do! (timeOutMillis 10) // delay 10ms between hits, don't be a jerk
+
+    // Return the status code and the response
+    return (response.statusCode, Response.readBodyAsString response |> run)
+}
+
 let addToInstapaper username password (urls : string list) =
     for url in urls do
         printfn "Submitting url %s" url
         try
             let request = createInstapaperRequest username password url
-            let resp = request |> Request.responseAsString |> run
-            printfn "Committed %s and got Response=%s" url resp
+            let resp = request |> sendRequestToInstapaper |> run
+            printfn "Committed %s and got Response code=%i, body=%s" url (fst resp) (snd resp)
         with
             | ex -> printfn "Exception %s" (ex.GetBaseException()).Message
     ()
@@ -47,8 +55,8 @@ let main argv =
     let baseUrl = "http://edmunds.com"
     let fullUrls = List.map(fun x -> baseUrl + x) urls
         
-    let username = "foobar"
-    let password = "foobar"
+    let username = "username"
+    let password = "password"
 
     // Write to file
     addToInstapaper username password fullUrls
